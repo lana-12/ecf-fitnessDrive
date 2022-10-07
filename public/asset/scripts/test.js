b773fff24586b5a3d48945cc067676511b395146
Virginie Giacometti <lana.12@wanadoo.fr>
2022-10-07 12:52:08 +0200

    add js

--- public/asset/scripts/test.js
+++ public/asset/scripts/test.js
@@ -1,20 +1,3 @@
 console.log('hello');
 console.log('un autre test');
-// alert('test ok')
-// let btntest= document.getElementById('test');
-
-// function show(){
-//     alert('test ok');
-// };
-// btntest.addEventListener(click, show())
-
-
-// function displayPassword(){
-//     let x = document.getElementById("id_password");
-//         if (x.type === "password") {
-//             x.type = "text";
-//         } else {
-//             x.type = "password";
-//         }
-// }
 
